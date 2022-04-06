51b5a057fbb51a9da1a92214c42ca7312036f22f
Sergi S <sergi.santos.7e4@itb.cat>
2022-04-07 00:40:54 +0200

    .

--- sketch.js
+++ sketch.js
@@ -65,8 +65,8 @@ function draw() { // this function code runs in infinite loop
         // Apply specs and cigar
         //image(specs, singlePose.nose.x-40, singlePose.nose.y-70, 125, 125);
         //image(smoke, singlePose.nose.x-35, singlePose.nose.y+28, 50, 50);
-        image(irritedeyes, singlePose.leftEye.x-10, singlePose.leftEye.y+10, 25,25);
-        image(irritedeyes, singlePose.rightEye.x-10, singlePose.rightEye.y+10, 25,25);
+        image(irritedeyes, singlePose.leftEye.x-10, singlePose.leftEye.y-10, 25,25);
+        image(irritedeyes, singlePose.rightEye.x-10, singlePose.rightEye.y-10, 25,25);
     }
     
     //background(200);
